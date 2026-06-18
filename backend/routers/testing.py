9082bbebe6caca4f3b460bbfff579a565dd52482
priyanshuranjan27 <priyanshu.ranjan-ext@hpe.com>
2026-06-18 19:02:05 +0530

    remove the dummy part

--- backend/routers/testing.py
+++ backend/routers/testing.py
@@ -1,13 +1,34 @@
 from fastapi import APIRouter
 from dotenv import load_dotenv
+import os
 from jira_connection import check_jira_connection
+import requests
 
 router=APIRouter()
 jira=check_jira_connection()  #contains jira acess...
 load_dotenv()
+
 
 @router.get("/testing")
 def testing():
     return {
         "message" :" this is testing inside backend/routers/testing.py"
     }
+
+def get_issues_between_dates(jira, project_key,start_date,end_date):
+    project_key=os.getenv('PROJECT_KEY')
+
+
+    jql_query=(
+        f'project = "{project_key}" '
+        f'AND created >= "{start_date}" '
+        f'AND created <= "{end_date}"'
+    )
+
+    issues=jira.search_issues(
+        jql_query,
+        maxResult=False
+    )
+
+
+
